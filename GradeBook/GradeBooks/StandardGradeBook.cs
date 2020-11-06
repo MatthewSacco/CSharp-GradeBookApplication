cbd397cf53a9ce850032760b8f53df573ed01b97
MatthewSacco <matthew.sacco@betssongroup.com>
2020-11-06 07:52:53 +0100

    more fixes

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -5,11 +5,11 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name)
+        public StandardGradeBook(string name) : base(name)
         {
-            Type = GradeBookType.Ranked;
+            Type = GradeBookType.Standard;
         }
     }
 }
